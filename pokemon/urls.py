0eace573398dbf36b32fa784207ef289235cbaa8
anna_joachimska <ajoachimska@bee2code.com>
2023-03-06 12:23:20 +0100

    rename ednpoints

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PokemonView, PokemonDetail, PokemonType, PokemonAbility
+from .views import PokemonView, PokemonDetail, AddOrRemoveTypeToPokemon, AddOrRemoveAbilityFromPokemon
 
 urlpatterns = [
     path('', PokemonView.as_view()),
     path('<int:pk>', PokemonDetail.as_view()),
-    path('add-or-remove-types/<int:pk>', PokemonType.as_view()),
-    path('add-or-remove-abilities/<int:pk>', PokemonAbility.as_view()),
+    path('add-or-remove-types/<int:pk>', AddOrRemoveTypeToPokemon.as_view()),
+    path('add-or-remove-abilities/<int:pk>', AddOrRemoveAbilityFromPokemon.as_view()),
 ]
