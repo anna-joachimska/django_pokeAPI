c5cef267d6b84bcd6fd715138197fd51f6a63187
anna_joachimska <ajoachimska@bee2code.com>
2023-03-07 17:20:46 +0100

    fix

--- pokemon_statistics/urls.py
+++ pokemon_statistics/urls.py
@@ -5,16 +5,16 @@ from .views import TypeWithHigestAvgHp, TypeWithHigestAvgAttack, TypeWithHigestA
     MostPopularAbility, MostPopularAbilityWithPokemons, PokemonAbilitiesCount
 
 urlpatterns = [
-    path('type/with-higest-hp', TypeWithHigestAvgHp.as_view()),
-    path('type/with-higest-attack', TypeWithHigestAvgAttack.as_view()),
-    path('type/with-higest-defense', TypeWithHigestAvgDefense.as_view()),
+    path('type/with-highest-hp', TypeWithHigestAvgHp.as_view()),
+    path('type/with-highest-attack', TypeWithHigestAvgAttack.as_view()),
+    path('type/with-highest-defense', TypeWithHigestAvgDefense.as_view()),
     path('type/count-pokemons/<int:pk>', PokemonTypesCount.as_view()),
     path('type/most-popular', MostPopularType.as_view()),
     path('type/most-popular-with-pokemons', MostPopularTypeWithPokemons.as_view()),
     path('type/count-pokemons-with-more-than-<int:X>-types', CountPokemonsWithMoreThanXType.as_view()),
-    path('ability/with-higest-hp', AbilityWithHigestAvgHp.as_view()),
-    path('ability/with-higest-attack', AbilityWithHigestAvgAttack.as_view()),
-    path('ability/with-higest-defense', AbilityWithHigestAvgDefense.as_view()),
+    path('ability/with-highest-hp', AbilityWithHigestAvgHp.as_view()),
+    path('ability/with-highest-attack', AbilityWithHigestAvgAttack.as_view()),
+    path('ability/with-highest-defense', AbilityWithHigestAvgDefense.as_view()),
     path('ability/count-pokemons/<int:pk>', PokemonAbilitiesCount.as_view()),
     path('ability/most-popular', MostPopularAbility.as_view()),
     path('ability/most-popular-with-pokemons', MostPopularAbilityWithPokemons.as_view()),
