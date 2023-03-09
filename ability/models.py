4c3d2fb774179b64180c4190b5d45b1ff3c31809
anna_joachimska <ajoachimska@bee2code.com>
2023-03-09 10:54:07 +0100

    removed blank = False

--- ability/models.py
+++ ability/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Ability(models.Model):
     id = models.AutoField(primary_key=True, editable=False, )
-    name = models.CharField(max_length=70, blank=False, unique=True)
+    name = models.CharField(max_length=70, unique=True)
 
     def __str__(self):
         return self.name
--- pokemon/models.py
+++ pokemon/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Pokemon(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
-    name = models.CharField(max_length=70, blank=False, unique=True)
+    name = models.CharField(max_length=70, unique=True)
     types = models.ManyToManyField('type.Type', blank=True)
     abilities = models.ManyToManyField('ability.Ability', blank=True)
     hp = models.IntegerField()
     attack = models.IntegerField()
     defense = models.IntegerField()
-    generation = models.CharField(max_length=70, blank=False)
+    generation = models.CharField(max_length=70)
 
     def __str__(self):
         return self.name
